64cd8b2196d1aa8ee8c900fb6529bd1109a2a3ac
Capoto <47067142+Capoto@users.noreply.github.com>
2021-01-25 19:52:05 -0300

    Create 1174.c
    https://www.urionlinejudge.com.br/judge/pt/problems/view/1174

---
+++ 1174.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main(){
+        
+        int i,y;
+        double x[100];
+
+     for(i=0;i<100;i++){   
+        scanf("%lf",&x[i]);
+}
+     for(y=0;y<100;y++){
+
+        if(x[y]<=10){
+
+printf("A[%d] = %.1lf\n",y,x[y]);
+}
+
+}   
+
+
+
+}
