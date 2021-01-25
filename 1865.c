2cff0576a575ac6c12c65c365a48944f1514e143
Capoto <47067142+Capoto@users.noreply.github.com>
2021-01-25 20:02:33 -0300

    Create 1865.c
    https://www.urionlinejudge.com.br/judge/pt/problems/view/1865

---
+++ 1865.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<string.h>
+void main(){
+
+         int x,c,d=0,soma,i;
+         char str[5000],strj[5]={"Thor"};
+     
+
+          scanf("%d",&x);
+
+                for(i=0;i<x;i++){
+                  scanf("%s%d",str,&c);
+
+                if(strcmp(str,strj)==0){
+
+                printf("Y\n");
+
+}
+
+                else{
+
+                printf("N\n");
+
+}           
+
+
+}
+
+
+}
