694a130ced11e45ad9be4a0093d33f9baa813556
Capoto <47067142+Capoto@users.noreply.github.com>
2021-01-25 11:58:12 -0300

    Create 2454.c
    https://www.urionlinejudge.com.br/judge/pt/problems/view/2454

---
+++ 2454.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main ()
+{int p,r;
+scanf("%d %d",&p,&r);
+if(p==1 && r==0){
+printf("B\n");}
+else if(p==0 ){
+printf("C\n");}
+if(p==1 && r==1){
+printf("A\n");}
+return 0;}
