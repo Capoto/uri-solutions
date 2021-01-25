ec79d0b2c6175ad30e1ede7c6f06a76cd034028d
Capoto <47067142+Capoto@users.noreply.github.com>
2021-01-25 10:49:26 -0300

    Create 1041.c
    https://www.urionlinejudge.com.br/judge/pt/problems/view/1041

---
+++ 1041.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int main()
+{
+    double X,Y;
+    scanf ("%lf%lf", &X, &Y);
+    if(X==0 && Y==0) 
+    {
+      printf("Origem\n");
+    }
+    if(X == 0 && Y != 0)
+    {
+      printf("Eixo Y\n");
+    }
+    if(Y == 0 && X != 0 )
+    { 
+      printf("Eixo X\n");
+    }
+    if(X>0 && Y>0)
+    {
+      printf("Q1\n");
+    }
+    if(X<0 && Y>0)
+    {
+      printf("Q2\n");
+    }
+    if(X<0 && Y<0) 
+    {
+      printf("Q3\n");
+      
+    }
+    if(X>0 && Y<0)
+    { 
+      printf("Q4\n");
+      
+    }
+    return 0;
+}
