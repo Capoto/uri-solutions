7ac7f4ac123c1919039b790f6c955b29c467ee1e
Capoto <47067142+Capoto@users.noreply.github.com>
2021-01-25 10:06:45 -0300

    Create 1000.c

---
+++ 1000.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+ printf("Hello World!\n");
+    return 0;
+}
