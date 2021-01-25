2c650a1f1f7bdd93dd9f192927126d68f1e485d3
Capoto <47067142+Capoto@users.noreply.github.com>
2021-01-25 17:26:20 -0300

    Create 1149.c
    https://www.urionlinejudge.com.br/judge/pt/problems/view/1149

---
+++ 1149.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+
+        void main(){
+
+                int a,n,b=0,i,j;
+                
+                        scanf("%d",&a);
+               
+                        scanf("%d",&n);
+                                if(n>0){
+                                                    for(i=0;i<n;i++){
+                                                                      
+                                       b+=a+i;
+
+}   
+
+
+        printf("%d\n",b);
+}
+                else if(n<=0){
+do{
+scanf("%d",&n);}while(n==0 || n<0);
+if(n>0){
+for(i=0;i<n;i++){
+                                                                      
+                                       b+=a+i;
+
+}   
+
+
+        printf("%d\n",b);
+}
+
+
+}
+}
