f5b931f2ef59100c4d52d0d2f9230b23240fd7b0
Capoto <47067142+Capoto@users.noreply.github.com>
2021-01-25 17:24:15 -0300

    Create 1145.c
    https://www.urionlinejudge.com.br/judge/pt/problems/view/1145

---
+++ 1145.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+
+        void main(){
+
+                int a,b,i,j;
+
+               scanf("%d",&a);
+               scanf("%d",&b);
+
+                        
+
+                            for(j=1,i=1;i<=a,j<=b;j++,i++){
+                                                             
+                                  
+                               if(i%a==0){  
+                               printf("%d",j);
+                               printf("\n");
+}
+                            else{
+                               printf("%d",j);
+                               printf(" ");  
+}
+}
+
+
+
+                        
+                          
+                        
+}
