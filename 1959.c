114cb4e9ec4919215d334268826b5b8d047cf53c
Capoto <47067142+Capoto@users.noreply.github.com>
2021-01-25 20:24:21 -0300

    Create 1959.c

---
+++ 1959.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main(){
+
+        long long int a,b,c,d,e,f,g,h,i,j;
+        
+        scanf("%lld%lld",&a,&b);
+
+        printf("%lld\n",a*b);
+
+                   
+
+
+}
