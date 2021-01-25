e84e26e460a003fefba20d90ecee21eb77c16c6f
Capoto <47067142+Capoto@users.noreply.github.com>
2021-01-25 14:05:39 -0300

    Create 1096.c
    https://www.urionlinejudge.com.br/judge/pt/problems/view/1096

---
+++ 1096.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+int I,J,x;
+
+ for(I=1;I<=9;I+=2)
+   
+   for(J=7;J>4;J--)
+   printf("I=%d J=%d\n",I,J);
+ 
+  if(I==9 && J==5){
+  return 0;}
+ 
+  
+  
+  
+  return 0;
+}
