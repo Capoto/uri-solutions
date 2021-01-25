a157bbdd06041281775996404d6166875c282a18
Capoto <47067142+Capoto@users.noreply.github.com>
2021-01-25 20:00:32 -0300

    Create 1759.c
    https://www.urionlinejudge.com.br/judge/pt/problems/view/1759

---
+++ 1759.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void main(){
+
+         int x,a,b,soma,i;
+
+          scanf("%d",&x);
+
+
+               for(i=1;i<=x;i++){
+               
+                  if(i!=x){
+                    printf("Ho ");}
+
+                  else{
+
+printf("Ho!\n");
+
+
+}
+
+
+               
+}      
+
+
+
+
+}
