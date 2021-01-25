2c63f57a25aec2c467f7d058d8f073d92dae2882
Capoto <47067142+Capoto@users.noreply.github.com>
2021-01-25 19:59:02 -0300

    Create 1564.c
    https://www.urionlinejudge.com.br/judge/pt/problems/view/1564

---
+++ 1564.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main(){
+
+         int x;
+
+                while(scanf("%d",&x) !=EOF){
+
+if(x==0){
+
+printf("vai ter copa!\n");
+}
+else{
+printf("vai ter duas!\n");
+
+}
+
+
+}
+
+
+
+
+
+}
