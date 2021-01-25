35c931cff8d66154f26508d5e92b078676c49456
Capoto <47067142+Capoto@users.noreply.github.com>
2021-01-25 19:59:50 -0300

    Create 1589.c
    https://www.urionlinejudge.com.br/judge/pt/problems/view/1589

---
+++ 1589.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main(){
+
+         int x,a,b,soma,i;
+
+          scanf("%d",&x);
+
+
+               for(i=0;i<x;i++){
+
+               scanf("%d%d",&a,&b);
+
+               soma = a+b;
+  
+               printf("%d\n",soma);  
+}      
+
+
+
+
+}
