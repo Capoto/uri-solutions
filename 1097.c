17be764812bf50bad1a8f4ef9c5256ce9437c144
Capoto <47067142+Capoto@users.noreply.github.com>
2021-01-25 14:10:03 -0300

    Create 1097.c
    https://www.urionlinejudge.com.br/judge/pt/problems/view/1097

---
+++ 1097.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main(){
+int I,J,x;
+
+
+   for(J=7;J>4;J--){
+     I=1;
+   
+   printf("I=%d J=%d\n",I,J);}
+ 
+
+  for(J=9;J>6;J--){
+    I=3;
+
+printf("I=%d J=%d\n",I,J);
+  }
+  for(J=11;J>8;J--){
+    
+    I=5;
+    printf("I=%d J=%d\n",I,J);
+  }
+   for(J=13;J>10;J--){
+      I=7;
+     
+      printf("I=%d J=%d\n",I,J);
+   }
+   for(J=15;J>12;J--){
+     
+     I=9;
+     
+      printf("I=%d J=%d\n",I,J);
+   }
+  
+  
+  
+  
+  return 0;
+}
