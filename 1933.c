b57bf295828e9d8ea057e2e3878047963f2b66a9
Capoto <47067142+Capoto@users.noreply.github.com>
2021-01-25 20:04:05 -0300

    Create 1933.c
    https://www.urionlinejudge.com.br/judge/pt/problems/view/1933

---
+++ 1933.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+void main(){
+
+         int x,c,d=0,soma=0,i;
+         
+         scanf("%d%d",&x,&c);
+
+         if(x>c){
+
+         printf("%d\n",x);
+}
+        else if(c>x){
+
+         printf("%d\n",c);
+}
+
+      else{
+
+        printf("%d\n",x);
+}
+
+
+
+}
