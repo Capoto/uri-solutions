6321ceb4b5dc877fb287850949cf1403653e7d22
Capoto <47067142+Capoto@users.noreply.github.com>
2021-01-25 20:15:25 -0300

    Create 1921.c

---
+++ 1921.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void main(){
+
+        long long int b;
+        
+                
+        scanf("%lld",&b);
+
+        printf("%lld\n",(b*(b-3))/2); 
+
+
+        
+        
+}
