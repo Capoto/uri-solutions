2ba9de60e28f05a495f902d18b2a057564eb9e57
Capoto <47067142+Capoto@users.noreply.github.com>
2021-01-25 19:55:00 -0300

    Create 1178.c
    https://www.urionlinejudge.com.br/judge/pt/problems/view/1178

---
+++ 1178.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<math.h>
+void main(){
+        
+        double x,y,j,p;
+        int i,h;
+        scanf("%lf",&x);
+        
+                for(i=0;i<100;i++){
+        
+                       
+                  p = x*pow(0.5,i);
+                
+         
+printf("N[%d] = %.4lf\n",i,p);
+
+
+               
+
+
+          
+}
+
+
+
+
+
+
+}
