55f29c903b86c7b472f678397ef89d4d24b07a4c
Capoto <47067142+Capoto@users.noreply.github.com>
2021-01-25 10:30:11 -0300

    Create 1011.c
    https://www.urionlinejudge.com.br/judge/pt/problems/view/1011

---
+++ 1011.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include<stdlib.h>
+ int main() {
+        double R,x;
+       scanf("%lf",&R);
+        x =((4)*( 3.14159)*(R*R*R))/3;
+          printf("VOLUME = %.3lf\n",x);
+           return 0; }
