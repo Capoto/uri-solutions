8524d45d0e8fae1eadee2d69035b038a1782d33a
Capoto <47067142+Capoto@users.noreply.github.com>
2021-01-25 20:27:41 -0300

    Create 2006.c

---
+++ 2006.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main(){
+
+        int a,b,c=0,d,e,f,g,h,i,j;
+        
+        scanf("%d",&a);
+
+        for(i=0;i<5;i++){
+             scanf("%d",&b);
+                   if(b==a){
+c++;
+}
+                       
+}
+        printf("%d\n",c);
+                   
+
+
+}
