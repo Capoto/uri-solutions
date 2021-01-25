376e602ac8230f781b4e23fca0b50b73ae72d3a0
Capoto <47067142+Capoto@users.noreply.github.com>
2021-01-25 17:25:21 -0300

    Create 1146.c
    https://www.urionlinejudge.com.br/judge/pt/problems/view/1146

---
+++ 1146.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+
+        void main(){
+
+                int a,b,i,j;
+                while(1){
+                        
+
+                        scanf("%d",&a);
+               
+                                if(a==0){break;}
+
+
+                    for(i=1;i<=a;i++){
+if(i!=a){
+printf("%d ",i);}
+else{
+printf("%d",i);
+}
+
+}
+printf("\n");
+                        
+                          
+                        }
+}
