602c41b1b1cd74d7426d37b3b93ab1af7e50478d
Capoto <47067142+Capoto@users.noreply.github.com>
2021-01-25 13:34:36 -0300

    Create 1113.c
    https://www.urionlinejudge.com.br/judge/pt/problems/view/1113

---
+++ 1113.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+            int x,b;
+    
+
+ while(x==b){
+scanf("%d%d",&x,&b);
+break;}
+while(x!=b){
+scanf("%d%d",&x,&b);
+if(x>b){
+printf("Decrescente\n");}
+else if(x<b){
+printf("Crescente\n");}}
+ 
+
+
+
+
+return 0;}
