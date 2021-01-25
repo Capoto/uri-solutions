d0b8f8733c031b41df1d7bba54bd8934bc1c221a
Capoto <47067142+Capoto@users.noreply.github.com>
2021-01-25 17:23:20 -0300

    Create 1144.c
    https://www.urionlinejudge.com.br/judge/pt/problems/view/1144

---
+++ 1144.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+
+        void main(){
+
+                int a,b=1,c=1,d=1,e=0,f=0,g=0,i;
+
+               scanf("%d",&a);
+               
+
+                        for(i=0;i<a*2;i++){
+                             
+                              printf("%d %d %d\n",b,c,d);
+                              e++;
+                              f++;
+                              g++;  
+                                  if(e%2!=0){
+                                 c++;
+                                 d++;
+  
+}
+                                 else{
+
+                                 b++;
+                                 c = b*b;
+                                 d = c*b;
+}  
+
+
+}
+                          
+                        
+}
