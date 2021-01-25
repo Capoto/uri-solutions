84e202fede4eb578f2c66ff06455baf4956870ea
Capoto <47067142+Capoto@users.noreply.github.com>
2021-01-25 20:08:47 -0300

    Create 1866.c

---
+++ 1866.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main(){
+
+        int a,b,i,soma;
+
+        scanf("%d",&a);
+
+
+                for(i=0;i<a;i++){
+
+            scanf("%d",&b);
+              if(b%2==0){
+
+soma=0;
+}
+else{
+soma=1;
+}
+printf("%d\n",soma);
+soma=0;
+
+}
+
+
+
+}
