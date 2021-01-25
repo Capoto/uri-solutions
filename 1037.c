0cc2c39c9b8cf0fc065c60a505f46b5634d53142
Capoto <47067142+Capoto@users.noreply.github.com>
2021-01-25 10:43:50 -0300

    Create 1037.c
    https://www.urionlinejudge.com.br/judge/pt/problems/view/1037

---
+++ 1037.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int main()
+{
+    double a;
+    scanf ("%lf",&a);
+    if(a<0||a>100)
+    {
+        printf ("Fora de intervalo\n");
+    }
+  if(a>=0 && a<=25)
+  {
+        printf ("Intervalo [0,25]\n");
+  }
+  if(a>25 && a<=50)
+  {
+        printf ("Intervalo (25,50]\n");
+  }
+  if (a>50 && a<=75)
+  {
+        printf ("Intervalo (50,75]\n");
+  }
+  if (a>75 && a<=100)
+  {
+        printf ("Intervalo (75,100]\n");
+    }
+    return 0;
+}
