5a98971a17052a01d7874e24ccdfa80a250f76b6
Capoto <47067142+Capoto@users.noreply.github.com>
2021-01-25 17:21:21 -0300

    Create 1133.c
    https://www.urionlinejudge.com.br/judge/pt/problems/view/1133

---
+++ 1133.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+
+        void main(){
+
+                int a,b,i;
+
+               scanf("%d",&a);
+               scanf("%d",&b);
+
+                if(a>=b){  
+                          for(i=b+1;i<a;i++){
+                                if(i%5==2 || i%5==3){
+
+                                     printf("%d\n",i); 
+
+}
+
+}
+}
+                if(b>a){  
+                          for(i=a+1;i<b;i++){
+                                if(i%5==2 || i%5==3){
+
+                                   printf("%d\n",i);  
+
+}
+
+}
+}
+                        
+}
