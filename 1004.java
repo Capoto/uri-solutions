d457096a1a210afc35edab339e8d7b6354d8f9f0
Capoto <47067142+Capoto@users.noreply.github.com>
2021-01-28 13:08:24 -0300

    Create 1004.java

---
+++ 1004.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+ 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+import java.util.Scanner;
+/**
+ *
+ * @author gomes
+ */
+public class Main {
+    
+    public static void main(String[] args){
+        Scanner input  = new Scanner(System.in);
+        int z,x,y;
+        
+        x = input.nextInt();
+        y = input.nextInt();
+        z = x*y;
+        System.out.println("PROD = " + z);
+    }
+    
+}
